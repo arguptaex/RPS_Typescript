00d8c3db7664fb8f5a896c21d92db7d2d0604972
arguptaex <architgupta.rke@gmail.com>
2024-02-12 12:11:46 +0530

    RPS_typescript_Game: TS file added

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+console.log("hello rps");
+function rpsGame(round_no, scoreboard, player_move) {
+    let playerScore;
+    let compScore;
+    let result;
+    let moves = ['fire', 'water', 'grass'];
+    let options = {
+        "fire": "grass",
+        "grass": "water",
+        "water": "fire"
+    };
+    let compMove = "fire";
+    if (player_move == compMove) {
+        result = "Its a Tie; Try again !";
+        console.log("Its a Tie; Try again !");
+    }
+    else if (options[player_move] == compMove) {
+        result = "You Won";
+        console.log("You won");
+    }
+    else {
+        result = "You Loose";
+        console.log("You Loose");
+    }
+    return [playerScore, compScore, result];
+}
+;
+function startGame() {
+    // Get player name input value
+    const playerNameInput = document.getElementById("player_name");
+    const playerName = playerNameInput.value;
+    console.log(playerNameInput.value);
+    // Redirect to another page passing player name as query parameter
+    window.location.href = `/gameplay.html`;
+    // window.location.href = "wwww.google.com"
+}
